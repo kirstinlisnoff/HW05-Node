ba97ece3886c5f1970e4624d39cbfe8abb88871a
Kiki <kirstinlisnoff5@gmail.com>
2025-02-12 14:02:06 -0500

    created function to generate readme file

--- index.js
+++ index.js
@@ -7,41 +7,54 @@ const questions = [
     {
         type: "input",
         message: "What is the title of your project?",
-        name: title
+        name: "title"
     },
     {
         type: "input",
         message: "Please provide a short description explaining your project.",
-        name: description
+        name: "description"
     },
     {
         type: "input",
         message: "What are the steps for installation?",
-        name: installation
+        name: "installation"
     },
     {
         type: "input",
         message: "Please provide instructions for usage of the application.",
-        name: usage
+        name: "usage"
     },
     {
         type: "input",
         message: "Pleast list any contributors to the application.",
-        name: credits
+        name: "credits"
     },
     {
        type: "list",
        message: "Please select the licensing option best suited to your project.",
-       name: license,
+       name: "license",
        choices: ["MIT","GPL","AFL","Apache-2.0","BSL-1.0","ISC","None"]
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync('README.txt', data, (err) => {
+        err ? console.error(err) : console.log('README Created!');
+      });
+}
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(response => {
+        writeToFile("README.txt", `
+          #${response.title} 
+            `)
+    })
+}
 
 // Function call to initialize app
 init();
